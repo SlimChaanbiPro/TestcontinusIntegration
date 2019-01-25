008e6293d563103c9d1165faf5121cb9ec0269bb
Slim Chaanbi <33354464+SlimChaanbiPro@users.noreply.github.com>
2019-01-25 18:14:32 +0100

    test3

--- src/main/java/dpc/com/domaine/Matiere.java
+++ src/main/java/dpc/com/domaine/Matiere.java
@@ -5,4 +5,29 @@ private int idmatiere;
 private String nom;
 private String nomber;
 private String departement;
+public int getIdmatiere() {
+	return idmatiere;
+}
+public void setIdmatiere(int idmatiere) {
+	this.idmatiere = idmatiere;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getNomber() {
+	return nomber;
+}
+public void setNomber(String nomber) {
+	this.nomber = nomber;
+}
+public String getDepartement() {
+	return departement;
+}
+public void setDepartement(String departement) {
+	this.departement = departement;
+}
+
 }
