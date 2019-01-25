c9fabb2620f53e195333285a23802969205664e0
Slim Chaanbi <33354464+SlimChaanbiPro@users.noreply.github.com>
2019-01-25 11:16:15 +0100

    intial commit

---
+++ src/main/java/dpc/com/demo/TestContinusIntegrationApplication.java
@@ -0,0 +1,14 @@
+package dpc.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContinusIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContinusIntegrationApplication.class, args);
+	}
+
+}
+
