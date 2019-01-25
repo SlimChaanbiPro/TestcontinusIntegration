b71bd3e6875179a573be5c6b5d49cb44fdbf96d9
wajdi077 <methlouthiwajdi@gmail.com>
2019-01-25 12:15:10 +0100

    gettes setters

--- src/main/java/dpc/com/domaine/Ville.java
+++ src/main/java/dpc/com/domaine/Ville.java
@@ -3,4 +3,14 @@ package dpc.com.domaine;
 public class Ville {
 	private String adresse ;
 
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	
+	
+
 }
